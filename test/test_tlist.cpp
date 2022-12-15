fa21de95ae2a9b894de571d9ca69917bdb31d941
4mada0 <4mada0@gmail.com>
2022-12-15 11:39:44 +0300

    added few tests

--- test/test_tlist.cpp
+++ test/test_tlist.cpp
@@ -2,3 +2,46 @@
 #include <gtest.h>
 #include <queue>
 
+TEST(TDynamicList, can_create)
+{
+	ASSERT_NO_THROW(TDynamicList<int> list);
+}
+
+TEST(TDynamicList, can_check_empty)
+{
+	TDynamicList<int> list;
+	EXPECT_EQ(list.empty(), true);
+}
+
+TEST(TDynamicList, can_create_copy)
+{
+	TDynamicList<int> list;
+	ASSERT_NO_THROW(TDynamicList<int> list2(list));
+}
+
+TEST(TDynamicList, can_push_front)
+{
+	TDynamicList<int> list;
+	ASSERT_NO_THROW(list.push_front(5));
+}
+
+TEST(TDynamicList, can_push_back)
+{
+	TDynamicList<int> list;
+	ASSERT_NO_THROW(list.push_back(5));
+}
+
+TEST(TDynamicList, can_insert_after_first)
+{
+	TDynamicList<int> list;
+	list.push_back(5);
+	ASSERT_NO_THROW(list.insert_after(list.front(), 78));
+}
+
+TEST(TDynamicList, can_erase_after_first)
+{
+	TDynamicList<int> list;
+	list.push_back(5);
+	list.push_back(15);
+	ASSERT_NO_THROW(list.erase_after(list.front()));
+}
