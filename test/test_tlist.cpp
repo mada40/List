ba08bd7280c1938f94754779d32097f340408f23
4mada0 <4mada0@gmail.com>
2022-12-15 12:41:54 +0300

    added more tests

--- test/test_tlist.cpp
+++ test/test_tlist.cpp
@@ -38,10 +38,107 @@ TEST(TDynamicList, can_insert_after_first)
 	ASSERT_NO_THROW(list.insert_after(list.front(), 78));
 }
 
+TEST(TDynamicList, can_return_front)
+{
+	TDynamicList<int> list;
+	list.push_back(5);
+	list.push_back(7);
+	EXPECT_EQ(list.front()->value, 5);
+}
+
+TEST(TDynamicList, can_return_back)
+{
+	TDynamicList<int> list;
+	list.push_back(5);
+	list.push_back(7);
+	EXPECT_EQ(list.back()->value, 7);
+}
+
+
 TEST(TDynamicList, can_erase_after_first)
 {
 	TDynamicList<int> list;
 	list.push_back(5);
 	list.push_back(15);
 	ASSERT_NO_THROW(list.erase_after(list.front()));
+}
+
+TEST(TDynamicList, can_erase_after_node)
+{
+	TDynamicList<int> list;
+	list.push_back(5);
+	list.push_back(15);
+	auto tmp = list.back();
+	list.push_back(35);
+	list.push_back(85);
+	ASSERT_NO_THROW(list.erase_after(tmp));
+}
+
+TEST(TDynamicList, can_insert_after_node)
+{
+	TDynamicList<int> list;
+	list.push_back(5);
+	list.push_back(15);
+	auto tmp = list.back();
+	list.push_back(35);
+	list.push_back(85);
+	ASSERT_NO_THROW(list.insert_after(tmp, 666));
+}
+
+TEST(TDynamicList, erase_after_node_deletes_node)
+{
+	TDynamicList<int> list;
+	list.push_back(5);
+	list.push_back(15);
+	auto tmp = list.back();
+	list.push_back(35);
+	list.push_back(85);
+	list.erase_after(tmp);
+
+	std::queue<int> control;
+	control.push(5);
+	control.push(15);
+	control.push(85);
+
+	auto first = list.front();
+	while (first)
+	{
+		EXPECT_EQ(first->value, control.front());
+		control.pop();
+		first = first->next;
+	}
+}
+
+
+TEST(TDynamicList, insert_after_node_adds_node)
+{
+	TDynamicList<int> list;
+	list.push_back(5);
+	list.push_back(15);
+	auto tmp = list.back();
+	list.push_back(35);
+	list.push_back(85);
+	list.insert_after(tmp, 666);
+
+	std::queue<int> control;
+	control.push(5);
+	control.push(15);
+	control.push(666);
+	control.push(35);
+	control.push(85);
+
+	auto first = list.front();
+	while (first)
+	{
+		EXPECT_EQ(first->value, control.front());
+		control.pop();
+		first = first->next;
+	}
+}
+
+TEST(TDynamicList, can_pop_front)
+{
+	TDynamicList<int> list;
+	list.push_back(5);
+	ASSERT_NO_THROW(list.pop_front());
 }
